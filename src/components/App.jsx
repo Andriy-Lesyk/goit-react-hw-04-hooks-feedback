80a01984ebadc9830f24ce2ebb62ae30d989b471
Андрій <74345435+Andriy-Lesyk@users.noreply.github.com>
2022-04-29 21:50:11 +0300

    змінює homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export default function App ()  {
-  const [good, setGood]=useState(0)
-  const [neutral, setNeutral]=useState(0)
-  const [bad, setBad]=useState(0)
-  
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   const handleIncrement = event => {
-    switch(event.target.name){
+    switch (event.target.name) {
       case 'good':
         setGood(prevState => prevState + 1);
         break;
@@ -23,39 +23,32 @@ export default function App ()  {
         setBad(prevState => prevState + 1);
         break;
 
-    default:
-      return;
+      default:
+        return;
     }
-  }
-  
+  };
 
-  const handleIncrementTotal = () =>
-    ( good + bad + neutral);
+  const total = good + bad + neutral;
+  const handleIncrementTotal = () => total;
 
-  const handleIncrementPositive = () =>
-    ( Math.round((good/(good+bad+neutral) ) * 100));
+  const handleIncrementPositive = () => Math.round((good / total) * 100);
 
-  
-    
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            handleFeedback={handleIncrement}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions handleFeedback={handleIncrement} />
+        {good !== 0 || bad !== 0 || neutral !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={handleIncrementTotal()}
+            positivePercentage={handleIncrementPositive()}
           />
-          {good !== 0 || bad !== 0 || neutral !== 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={handleIncrementTotal()}
-              positivePercentage={handleIncrementPositive()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+}
